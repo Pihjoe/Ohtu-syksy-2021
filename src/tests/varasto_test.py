65fb78aeb5557965cda0733977ec1d152a47b285
Joel <joel.pihlaja@helsinki.fi>
2021-11-15 19:12:17 +0200

    tehtävä 1.9

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -6,7 +6,8 @@ class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
 
-    def test_konstruktori_luo_tyhjan_varaston(self):
+    def 
+    test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
